b7608c5d847adecbef7320a57c3004db4b65b1da
Neil Patel <neilpatel10@yahoo.com>
2020-10-09 19:52:19 -0400

    the start of the dashboard
    no errors pog

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Tabs, Tab, Container, Row, Col, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <Navbar bg="primary" expand="lg" variant="dark">
+              <Navbar.Brand>Klemis Kitchen Dashboard</Navbar.Brand>
+            </Navbar>
+          </Col>
+          <Col>
+            <Button variant="danger">
+              Log Out
+            </Button>
+          </Col>
+        </Row>
+        {RenderTabs()}
+      </Container>
     </div>
+  );
+}
+
+function RenderTabs() {
+  const [key, setKey] = useState('home');
+
+  return (
+    <Tabs
+      activeKey={key}
+      onSelect={(k: any) => setKey(k)}
+    >
+      <Tab eventKey="home" title="Home">
+        1
+      </Tab>
+      <Tab eventKey="members" title="Members">
+        2
+      </Tab>
+      <Tab eventKey="details" title="ItemDetails">
+        3
+      </Tab>
+      <Tab eventKey="announcements" title="Announcements">
+        4
+      </Tab>
+    </Tabs>
   );
 }
 
